97dafac0706830c4427a386d1b4e7c2dd64e0ba9
xgg0213 <m.eringan@gmail.com>
2024-10-07 12:22:08 -0700

    completed

---
+++ src/components/Message.jsx
@@ -0,0 +1,46 @@
+
+import { useEffect, useState } from 'react';
+
+function Message({ size, featherCount }) {
+  const [sizeClass, setSizeClass] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    console.log('Message', size);
+
+    let cname = '';
+    switch (size) {
+      case 'm':
+        cname = 'medium';
+        break;
+      case 'l':
+        cname = 'large';
+        break;
+      case 'xl':
+        cname = 'xlarge';
+        break;
+      default:
+        cname = 'small';
+        break;
+    }
+    setSizeClass(cname);
+  }, [size]);
+
+  useEffect(()=> {
+    if (featherCount <= 0) {
+      setMessage('Oh my! Your bird is naked!');
+    } else if (featherCount >= 10) {
+      setMessage('Full turkey!');
+    } else {
+      setMessage('Coming along...');
+    }
+  }, [featherCount])
+
+  return (
+    <div className={`message ${sizeClass}`}>
+      {message}
+    </div>
+  );
+}
+
+export default Message;
